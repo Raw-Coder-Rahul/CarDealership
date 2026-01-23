bed157ca9fb7ecf880460591b7dc9ab060474a09
Raw-Coder-Rahul <rb3335167@gmail.com>
2026-01-23 13:24:38 +0530

    Other Section

--- js/script.js
+++ js/script.js
@@ -31,3 +31,22 @@ var swiper = new Swiper(".myHome", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+/* counter section start */;
+let valueDisplay = document.querySelectorAll('.num');
+let interval = 1000;
+
+valueDisplay.forEach((valueDisplay) => {
+    
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-value"));
+
+    let duration = Math.floor(interval/endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
